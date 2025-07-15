30902d99f36f9522b84f4850375cf8fb312c61de
Justin Tan <tanjustin217@gmail.com>
2025-07-15 16:19:18 +0800

    Add project and Tech section

--- src/App.jsx
+++ src/App.jsx
@@ -8,20 +8,23 @@ import {reveal} from "./utils/animation"
 
 function App() {
 
-  function onScrollClick() {
+  function onExperienceClick() {
     document.getElementById("experience--section").scrollIntoView();
+  }
+  function onProjectsClick() {
+    document.getElementById("projects").scrollIntoView();
   }
   return (
     <main className="main">
       <motion.div className="container" initial="hiddenVariant" animate="revealedVariant" transition={{staggerChildren: 0.2, delayChildren:1.3}}>
-      <motion.img src={Justin} className="headshot"initial={{opacity:0}} animate={{opacity :1}} transition={{duration: 1}}/>
-      <motion.h1 className="subtext" initial={{y:50, opacity:0}} animate={{y:0,opacity:1}} transition={{ease:easeIn,type: "spring"}}>Hey! I'm Justin, a computer science student at the University of Calgary!</motion.h1>
+      <motion.h1 className="subtext" initial={{y:50, opacity:0}} animate={{y:0,opacity:1}} transition={{ease:easeIn,type: "spring"}}>Hey! Thanks for checking out my website!</motion.h1>
+      <motion.h2 className="subtext2" initial={{y:50, opacity:0}} animate={{y:0,opacity:1}} transition={{ease:easeIn,type: "spring"}}>I'm Justin, a computer science student at the University of Waterloo! I love creating <u>sites and apps</u> and learning new things! I also enjoy badminton🏸, ping pong🏓, and piano🎹.</motion.h2>
         <div className="icons">
         <motion.a href="https://github.com/JustinTan-1" target="_blank" whileHover={{scale:1.05}} variants={reveal}><img src={Github} className="icon" /></motion.a>
         <motion.a href="https://www.linkedin.com/in/justintan217/" target="_blank" whileHover={{scale:1.05}} variants={reveal}><img src={Linkedin} className="icon" /></motion.a>
         </div>
-        <motion.div className="links" whileHover={{scale:1.05}} variants={reveal} onClick={onScrollClick}>Experience</motion.div>
       </motion.div>
+
       <div id="experience--section">
       <motion.div className="experience" id="experience" initial={{opacity: 0, y:50}} whileInView={{y:0,opacity:1}} viewport={{once:true}} transition={{delay:0.2, duration:1, ease:easeIn,type: "spring"}}><h1 className="subtext">Experience</h1></motion.div>
       <div className="experience--container">
@@ -33,7 +36,69 @@ function App() {
       </motion.div>
       </div>
       </div>
-    </main>
+
+    <section id="skills">
+      <motion.h1 initial={{opacity: 0, y:50}} whileInView={{y:0,opacity:1}} viewport={{once:true}} transition={{delay:0.2, duration:1, ease:easeIn,type: "spring"}}>Technology</motion.h1>
+      <motion.div initial={{opacity: 0, y:50}} whileInView={{y:0,opacity:1}} viewport={{once:true}} transition={{delay:0.5, duration:1, ease:easeIn,type: "spring"}} className="skills-grid">
+        <div className="skill-item">
+          <img src="src/assets/React.png" alt="React" />
+        </div>
+        <div className="skill-item">
+          <img src="src/assets/JavaScript.png" alt="JavaScript" />
+        </div>
+        <div className="skill-item">
+          <img src="src/assets/Node.png" alt="Node.js" />
+        </div>
+        <div className="skill-item">
+          <img src="src/assets/Python.png" alt="Python" />
+        </div>
+        <div className="skill-item">
+          <img src="src/assets/Docker.png" alt="Docker" />
+        </div>
+        <div className="skill-item">
+          <img src="src/assets/Java.png" alt="Java" />
+        </div>
+        <div className="skill-item">
+          <img src="src/assets/PostgresSQL.png" alt="Postgres " />
+        </div>
+        <div className="skill-item">
+          <img src="src/assets/Flask.png" alt="Flask" />
+        </div>
+        <div className="skill-item">
+          <img src="src/assets/PyTorch.png" alt="PyTorch" />
+        </div>
+        <div className="skill-item">
+          <img src="src/assets/Git.png" alt="Git" />
+        </div>
+        <div className="skill-item">
+          <img src="src/assets/HTML5.png" alt="HTML" />
+        </div>
+        <div className="skill-item">
+          <img src="src/assets/CSS3.png" alt="CSS" />
+        </div>
+        <div className="skill-item">
+          <img src="src/assets/scikit-learn.png" alt="sci-kit learn" />
+        </div>
+        <div className="skill-item">
+          <img src="src/assets/OpenCV.png" alt="OpenCV" />
+        </div>
+      </motion.div>
+    </section>
+
+  <section id="projects">
+    <motion.h1 initial={{opacity: 0, y:50}} whileInView={{y:0,opacity:1}} viewport={{once:true}} transition={{delay:0.2, duration:1, ease:easeIn,type: "spring"}}>Projects</motion.h1>
+    <motion.div initial={{opacity: 0, y:50}} whileInView={{y:0,opacity:1}} viewport={{once:true}} transition={{delay:0.5, duration:1, ease:easeIn,type: "spring"}} class="projects-grid">
+      <div class="project-card">
+        <h3>Math Recognition <a href="https://github.com/JustinTan-1/math-recognition" target="_blank"><img src="src/assets/GitHub.png" className="github--redirect"></img></a></h3>
+        <p>Chrome extension that allows users to draw math symbols and copy their desired symbol. </p>
+      </div>
+      <div class="project-card">
+          <h3>DiscordX <a href="https://github.com/JustinTan-1/discordXAPI" target="_blank"><img src="src/assets/GitHub.png" className="github--redirect"></img></a></h3>
+        <p>Discord mass message management. Connects to your discord account and allows you to monitor channels and reply to messages easily with Google Gemini API.</p>
+      </div>
+    </motion.div>
+  </section>
+  </main> 
   )
 }
 
